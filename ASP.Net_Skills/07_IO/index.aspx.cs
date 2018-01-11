dcd1168bdfde7121a842ae098f9beca054d54f73
Keno12 <1598136216@qq.com>
2018-01-11 17:59:19 +0800

    IO

---
+++ ASP.Net_Skills/07_IO/index.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.Net_Skills._07_IO
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-iterate-through-a-directory-tree
+
+            //File
+            //string[] lines = System.IO.File.ReadAllLines(@"E:\0109链接 - 副本.xlsx", Encoding.Default);
+
+            //StreamReader
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"E:\0109链接 - 副本.xlsx",Encoding.Default);
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+
+        }
+    }
+}
---
+++ ASP.Net_Skills/07_IO/index2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.Net_Skills._07_IO
+{
+    public partial class index2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = @"E:\";
+            var txtFiles = Directory.EnumerateFiles(path, "*.txt");
+            foreach (var file in txtFiles)
+            {
+                string fileName = file.Substring(path.Length + 1);
+            }
+        }
+    }
+}
